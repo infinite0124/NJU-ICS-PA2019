e8f7222a5fff3982231d21aa25182ba021e7f907
tracer-pa-public <tracer@njuics.org>
2019-09-15 21:44:58 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  21:44:58 up  5:44,  1 user,  load average: 1.49, 1.39, 0.66 fa137577a7a60221e2d9a2b74f552c766e4b17c9

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -79,22 +79,21 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
-	printf("answer:%d",__ref_alu_adc(src,dest,data_size);
-	printf("/n");
+	//printf("answer:%d",__ref_alu_adc(src,dest,data_size);
+	//printf("/n");
 	return __ref_alu_adc(src, dest, data_size);
 		
 #else
 	//printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
 	uint32_t res=0;
-	res=src+dest;
+	res=src+dest+cpu.eflags.CF;
 	set_CF_add(res,src,data_size);
-	res+=cpu.eflags.CF;
 	set_PF(res);
 	set_ZF(res,data_size);
 	set_SF(res,data_size);
 	set_OF_add(res,src,dest,data_size);
-	printf("myan:%d",res);
-	printf("  ");
+	//printf("myan:%d",res);
+	//printf("  ");
 	return res&(0xFFFFFFFF>>(32-data_size));
 	//assert(0);
 	//return 0;
