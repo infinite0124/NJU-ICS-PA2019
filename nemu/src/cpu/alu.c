c5d584e0b1c81aab7f84986d9dba21db3426359b
monitor <tracer@njuics.org>
2019-09-16 09:22:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -96,7 +96,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 	//printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
 	uint32_t res=0;
 	res=src+dest+cpu.eflags.CF;
-	set_CF_adc(res,src+cpu.eflags.CF,data_size);
+	set_CF_adc(res,src,data_size);
 	set_PF(res);
 	set_ZF(res,data_size);
 	set_SF(res,data_size);
