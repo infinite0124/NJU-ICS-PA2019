cd197c21602f1060853cd046d6152da85464a7f6
tracer-pa-public <tracer@njuics.org>
2019-09-16 10:02:33 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  10:02:33 up  6:55,  1 user,  load average: 0.15, 0.10, 0.09 7c6ef8faaeecc074ff34a4d7a138004c7f572a0b

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -140,7 +140,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 	set_PF(res);
 	set_ZF(res,data_size);
 	set_SF(res,data_size);
-	set_OF_add(res,src,dest,data_size);
+	set_OF_adc(res,src,dest,data_size);
 	return res&(0xFFFFFFFF>>(32-data_size));
 	//assert(0);
 	//return 0;
