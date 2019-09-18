4830732abcff497a9d36b4a8160c9c4687b0c3bf
monitor <tracer@njuics.org>
2019-09-18 08:50:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -208,7 +208,7 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size)
 	//return __ref_alu_mul(src, dest, data_size);
 //#else
 	//printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	uint32_t res=0;
+	uint64_t res=0;
 	switch (data_size)
 	{
 		case 8:
@@ -223,7 +223,7 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size)
 			break;
 	}
 
-	return res&(0xFFFFFFFF>>(32-data_size));
+	return res&(0xFFFFFFFFFFFFFFFF>>(64-data_size));
 	//assert(0);
 	//return 0;
 //#endif
