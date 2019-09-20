873a947d16b16087dbd85d7207745c761d400dd7
tracer-pa-public <tracer@njuics.org>
2019-09-20 16:31:56 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  16:31:56 up 16:16,  1 user,  load average: 0.00, 0.00, 0.00 c3d80620c0221d3baebdd53c5965b61e443cac93

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -419,7 +419,7 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
 #else
 	//printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
 	//assert(0);
-	uint32_t res=(int)dest>>(int)src;
+	int32_t res=(int)dest>>(int)src;
 	set_CF_sar(src,dest,data_size);
 	set_PF(res);
 	set_ZF(res,data_size);
