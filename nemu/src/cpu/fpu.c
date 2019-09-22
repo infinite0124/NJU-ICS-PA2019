f8e522de55a17037d393ef8cf7cf2a43f4c79761
tracer-pa-public <tracer@njuics.org>
2019-09-22 14:56:06 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  14:56:06 up 1 day,  6:01,  1 user,  load average: 0.06, 0.03, 0.00 d722c431ae1db9271b5493ebc5bd188f6e55b47

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -334,9 +334,7 @@ uint32_t internal_float_mul(uint32_t b, uint32_t a)
 
 	/* TODO: exp_res = ? leave space for GRS bits. */
 	//printf("\e[0;31mPlease implement me at fpu.c\e[0m\n");
-	exp_res=fa.exponent+fb.exponent;
-	//if((fa.exponent>1)&&(fb.exponent>1))
-		exp_res-=127;
+	exp_res=fa.exponent-127+fb.exponent-26;
 	printf("exp_res=%x\n",exp_res);
 	//assert(0);
 	return internal_normalize(f.sign, exp_res, sig_res);
