91f029ee0f0b6929d096cfcc7f2b0e0b8493dcaf
tracer-pa-public <tracer@njuics.org>
2019-09-22 13:34:15 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  13:34:15 up 1 day,  4:40,  1 user,  load average: 0.02, 0.03, 0.01 1dde48bc1706faa47c3c331161b19e8f7eda40a1

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -79,7 +79,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 			//assert(0);
 			sticky=sig_grs&0x1;
 			sig_grs>>=1;
-			sig_grs=sig_grs&sticky;
+			sig_grs=sig_grs|sticky;
 		}
 	}
 	else if (exp == 0 && sig_grs >> (23 + 3) == 1)
