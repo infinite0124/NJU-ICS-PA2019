9fe5a56e51c2d79539760c16b603351733ccb45f
monitor <tracer@njuics.org>
2019-10-19 15:20:01 +0800

    git monitor commit.

---
+++ nemu/include/cpu/instr/adc.h
@@ -0,0 +1,9 @@
+#ifdef __INSTR_ADC_H__
+#define __INSTR_ADC_H__
+
+make_instr_func(adc_r2rm_b);
+make_instr_func(adc_r2rm_v);
+make_instr_func(adc_rm2r_b);
+make_instr_func(adc_rm2r_v);
+make_instr_func(adc_i2a_b);
+make_instr_func(adc_i2a_v);
