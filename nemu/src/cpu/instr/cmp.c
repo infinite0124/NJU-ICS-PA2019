5ce8c4e42d0ae8b9a3788bf08bfce3174eab3f4a
tracer-pa-public <tracer@njuics.org>
2019-10-19 19:18:28 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  19:18:28 up  9:49,  1 user,  load average: 0.02, 0.02, 0.00 5a9e01ad25972ecfd906f6cc22b8bd4dbd088b5

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -2,9 +2,9 @@
 
 static void instr_execute_2op()
 {
-	operand_read(&opr_src);
-	opr_dest.val-=sign_ext(opr_src.val,data_size);
-	operand_write(&opr_dest);
+	//operand_read(&opr_src);
+	//opr_dest.val-sign_ext(opr_src.val,data_size);
+	//operand_write(&opr_dest);
 }
 
 make_instr_impl_2op(cmp,i,rm,v);
