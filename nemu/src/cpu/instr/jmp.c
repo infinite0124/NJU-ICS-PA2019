3a11801a0ddf95c2054faa068091d274218df0c9
tracer-pa-public <tracer@njuics.org>
2019-10-20 23:47:10 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  23:47:10 up 1 day,  3:10,  1 user,  load average: 0.18, 0.08, 0.02 febb56e6b375b229f98eee2e5a54f17fe48566a5

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -74,7 +74,7 @@ make_instr_func(jle)
 	int len=2;
 	uint8_t imm=instr_fetch(eip+1,1);
 	if((cpu.eflags.SF!=cpu.eflags.OF)||(cpu.eflags.ZF!=0))
-		cpu.eip+=imm;
+		cpu.eip+=sign_ext(imm,32);
 	//printf("jle	%x\n",imm);
 	//print_asm_1("jle","",2,&imm);
 	return len;
