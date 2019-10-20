7471520d8cdbaac7fea56742b3d002c929674b08
monitor <tracer@njuics.org>
2019-10-20 16:30:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -3,15 +3,17 @@
 int call_rel(uint32_t eip,uint8_t opcode)
 {
 	int len=1;
-	push_eip();
+	//push_eip();
 	//printf("esp=%x\n",cpu.esp);
 	uint32_t imm=instr_fetch(eip+1,data_size/8);
 	//printf("original_eip=%x\n",cpu.eip);
 	//printf("imm=%x\n",imm);
 	len+=data_size/8;
+	cpu.eip+=8*len;
+	push_eip();
 	cpu.eip=cpu.eip+imm;
 	if(data_size==16)
 		cpu.eip=cpu.eip&0x0000ffff;
 	//printf("eip=%x\n",cpu.eip);
-	return len;
+	return 0;
 }
