0e957687f8bedaa98d230d5cd939f9b4cee0061c
monitor <tracer@njuics.org>
2019-10-21 00:01:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -75,8 +75,8 @@ make_instr_func(jle)
 	int8_t imm=instr_fetch(eip+1,1);
 	if((cpu.eflags.SF!=cpu.eflags.OF)||(cpu.eflags.ZF!=0))
 		cpu.eip+=sign_ext(imm,32);
-	printf("jle	%x\n",imm);
-	printf("eip=%x\n",cpu.eip);
+	//printf("jle	%x\n",imm);
+	//printf("eip=%x\n",cpu.eip);
 	//print_asm_1("jle","",2,&imm);
 	return len;
 }
