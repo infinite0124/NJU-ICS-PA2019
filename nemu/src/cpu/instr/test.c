9b452c39adc9cdd7d17faa4c105028aa01db9b9c
monitor <tracer@njuics.org>
2019-10-20 19:02:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -5,7 +5,7 @@ static void instr_execute_2op()
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
 	alu_and(opr_src.val,opr_dest.val,data_size);
-	printf("ZF=%x\n",cpu.eflags.ZF);
+//	printf("ZF=%x\n",cpu.eflags.ZF);
 }
 
 make_instr_impl_2op(test,r,rm,v)
