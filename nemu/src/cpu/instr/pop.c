34165e3e13388a664af473d071db6d5fd55fa32f
tracer-pa-public <tracer@njuics.org>
2019-10-20 15:58:14 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  15:58:14 up 20:47,  1 user,  load average: 0.00, 0.04, 0.01 4dd8460a390199d0a48592bba4000ccebc995bb2

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -3,9 +3,10 @@
 int pop()
 {
 	OPERAND imm;
-	imm.type=OPR_MEM;
+	imm.type=OPR_IMM;
 	imm.data_size=data_size;
 	imm.addr=cpu.esp;
+	printf("esp=%x\n",cpu.esp);
 	operand_write(&imm);
 	cpu.esp+=data_size;
 	return imm.val;
