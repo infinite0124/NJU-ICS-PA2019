01a11cb9755e3720da9c62c1f4c9b9ba79ea0a62
tracer-pa-public <tracer@njuics.org>
2019-10-20 19:27:51 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  19:27:51 up 23:22,  1 user,  load average: 0.02, 0.02, 0.00 44fc5b077b1f174a6de269f8e378a090cc669317

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -2,11 +2,11 @@
 
 static void instr_execute_2op()
 {
+	printf("eip=%x\n",cpu.eip);
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
 	opr_dest.val=alu_add(opr_src.val,opr_dest.val,data_size);
 	operand_write(&opr_dest);
-	printf("eip=%x\n",cpu.eip);
 }
 
 make_instr_impl_2op(add,r,rm,b)
