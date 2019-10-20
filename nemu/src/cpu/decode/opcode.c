b30547d3abda7c5a4d680194556eb6e9f32c1c2b
monitor <tracer@njuics.org>
2019-10-20 22:07:01 +0800

    git monitor commit.

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -77,7 +77,7 @@ instr_func group_1_v_entry[8] =
 
 /* 0x83 */
 instr_func group_1_bv_entry[8] =
-    {add_i2rm_v, or_i2rm_v, adc_i2rm_v, sbb_i2rm_v, and_i2rm_v, sub_i2rm_v, xor_i2rm_v, cmp_i2rm_v};
+    {add_i2rm_v, /*or_i2rm_v*/inv, /*adc_i2rm_v*/inv, /*sbb_i2rm_v*/inv,/* and_i2rm_v*/inv, sub_i2rm_v, /*xor_i2rm_v*/inv, cmp_i2rm_v};
 
 /* 0xc0 */
 instr_func group_2_b_entry[8] =
