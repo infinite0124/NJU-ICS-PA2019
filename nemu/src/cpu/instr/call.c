4e4908c7d872333cb62c034d4a0273a00d2a8d5e
tracer-pa-public <tracer@njuics.org>
2019-10-20 13:24:47 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  13:24:47 up 19:13,  1 user,  load average: 0.03, 0.03, 0.00 69e062a8cd166fab8efa0feaf451dd239d0964c

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -2,9 +2,12 @@
 
 int call_rel(uint32_t eip,uint8_t opcode)
 {
+	int len=1;
 	push_eip();
 	uint32_t imm=instr_fetch(eip+1,data_size);
 	cpu.eip+=imm;
 	if(data_size==16)
 		cpu.eip=cpu.eip&0x0000ffff;
+	len+=data_size;
+	return len;
 }
