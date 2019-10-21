d7c2995f0ee581c2e0de176d8e70cfbd011625e0
monitor <tracer@njuics.org>
2019-10-22 00:48:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -28,8 +28,8 @@ make_instr_func(jmp_rel)
 		imm.data_size=8;
 	imm.addr=eip+1;
 	operand_read(&imm);
-	cpu.eip+=imm.val;
-	print_asm_1("jmp","",1+imm.data_size/8,&imm);
+	print_asm_1("jmp","",1+data_size/8,&imm);
+	cpu.eip+=sign_ext(imm.val,data_size);
 	return 1+imm.data_size/8;
 }
 
