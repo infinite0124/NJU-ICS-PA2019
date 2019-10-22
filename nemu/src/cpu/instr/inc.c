b4fa8edccbf146b87f408f547db947d3ac6b9deb
monitor <tracer@njuics.org>
2019-10-22 19:11:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -3,8 +3,10 @@
 static void instr_execute_1op()
 {
 	operand_read(&opr_src);
-	opr_src.val=alu_add(1,opr_src.val,data_size);
+	printf("opr_src.val=%x\n",opr_src.val);
+	opr_src.val=alu_add(0x1,opr_src.val,data_size);
 	operand_write(&opr_src);
+	printf("opr_src.val=%x\n",opr_src.val);
 }
 
 make_instr_impl_1op(inc,rm,v);
