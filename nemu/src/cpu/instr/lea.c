0de0260a55a76e3194fae97741351f5624c49c45
monitor <tracer@njuics.org>
2019-10-22 23:17:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -2,7 +2,7 @@
 
 static void instr_execute_2op()
 {
-	printf("base=%x,scale=%x,index=%x\n",opr_src.mem_addr.base,opr_src.mem_addr.scale,opr_src.mem_addr.index);
+	//printf("base=%x,scale=%x,index=%x\n",opr_src.mem_addr.base,opr_src.mem_addr.scale,opr_src.mem_addr.index);
 	//opr_dest.val=cpu.gpr[opr_src.mem_addr.base].val+cpu.gpr[opr_src.mem_addr.index].val*opr_src.mem_addr.scale;
 	opr_dest.val=opr_src.addr;
 	operand_write(&opr_dest);
