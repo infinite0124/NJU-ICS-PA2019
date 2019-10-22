9abcd56dc5fe052aa622379099dbd2a3e769a37d
tracer-pa-public <tracer@njuics.org>
2019-10-22 18:16:12 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  18:16:12 up 1 day, 11:54,  1 user,  load average: 0.10, 0.07, 0.01 46a46622dc2eed28ef16e6262796bdccc023e4f5

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -4,13 +4,13 @@ make_instr_func(leave)
 {
 	if(data_size==16)
 	{
-		cpu.gpr[4]_16=cpu.gpr[5]_16;
-		cpu.gpr[5]_16=pop();
+		cpu.gpr[4]._16=cpu.gpr[5]._16;
+		cpu.gpr[5]._16=pop();
 	}
 	else
 	{
-		cpu.gpr[4]_32=cpu.gpr[5]_32;		
-		cpu.gpr[5]_32=pop();
+		cpu.gpr[4]._32=cpu.gpr[5]._32;		
+		cpu.gpr[5]._32=pop();
 	
 	}
 	return 1;
