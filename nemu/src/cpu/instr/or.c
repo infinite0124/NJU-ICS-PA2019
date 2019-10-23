4a77dcd02ec2a06258373269b32c847896bf92ba
monitor <tracer@njuics.org>
2019-10-23 18:34:02 +0800

    git monitor commit.

---
+++ nemu/src/cpu/instr/or.c
@@ -0,0 +1,16 @@
+#include "cpu/instr.h"
+
+static void instr_execute_2op()
+{
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	opr_dest.val=alu_or(opr_src.val,opr_dest.val,data_size);
+	operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(or,r,rm,b)
+make_instr_impl_2op(or,r,rm,v)
+make_instr_impl_2op(or,rm,r,b)
+make_instr_impl_2op(or,rm,r,v)
+make_instr_impl_2op(or,i,a,b)
+make_instr_impl_2op(or,i,a,v)
