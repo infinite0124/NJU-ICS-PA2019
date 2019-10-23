7995d1031771b3e8c8fd9162201a702e91b141f3
tracer-pa-public <tracer@njuics.org>
2019-10-23 21:21:20 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  21:21:20 up 1 day, 23:10,  1 user,  load average: 0.20, 0.07, 0.03 f786f7492f1c3b1364144676e1da1dc8ec218a3

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -15,4 +15,6 @@ make_instr_impl_2op(cmp,r,rm,v);
 make_instr_impl_2op(cmp,r,rm,b);
 make_instr_impl_2op(cmp,rm,r,b);
 make_instr_impl_2op(cmp,rm,r,v);
+make_instr_impl_2op(cmp,i,a,b);
+make_instr_impl_2op(cmp,i,a,v)
 
