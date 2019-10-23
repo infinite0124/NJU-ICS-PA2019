f53acf8d60aa8f7db919dc274ed77b2beb08dc5e
tracer-pa-public <tracer@njuics.org>
2019-10-23 19:50:13 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  19:50:13 up 1 day, 22:03,  1 user,  load average: 0.06, 0.08, 0.08 106fd9ba9313b2845b521c984b79c181a139b325

--- nemu/src/cpu/instr/not.c
+++ nemu/src/cpu/instr/not.c
@@ -3,7 +3,9 @@
 static void instr_execute_1op()
 {
 	operand_read(&opr_src);
+	printf("addr=%x,oval=%x\n",opr_src.addr,opr_src.val);
 	opr_src.val=~opr_src.val;
+	printf("val=%x\n",opr_src.val);
 	operand_write(&opr_src);
 }
 
