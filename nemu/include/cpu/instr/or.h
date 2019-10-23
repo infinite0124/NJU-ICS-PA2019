867efe1ea048b606d28174a62eb8fd93577bfa6d
tracer-pa-public <tracer@njuics.org>
2019-10-23 21:05:59 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  21:05:59 up 1 day, 22:54,  1 user,  load average: 0.02, 0.04, 0.07 88c0a0d13c6da01e0e0ecffa1ac1cbb59a64cb0

--- nemu/include/cpu/instr/or.h
+++ nemu/include/cpu/instr/or.h
@@ -7,7 +7,7 @@ make_instr_func(or_rm2r_b);
 make_instr_func(or_rm2r_v);
 make_instr_func(or_i2a_b);
 make_instr_func(or_i2a_v);
-make_instr_func(or_i2rm_b);
+make_instr_func(or_i2rm_bv);
 make_instr_func(or_i2rm_v);
 
 #endif
