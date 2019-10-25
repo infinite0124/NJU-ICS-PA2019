026a72a828f45cb239b26d2e248b146ef4ac26f5
tracer-pa-public <tracer@njuics.org>
2019-10-25 18:33:09 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  18:33:09 up 2 days,  6:00,  1 user,  load average: 0.00, 0.05, 0.02 c7f28301b12e16a7a53bfeba6ba486a346cddb7

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -17,8 +17,8 @@ instr_func opcode_entry[256] = {
     /* 0x34 - 0x37*/ xor_i2a_b, xor_i2a_v, inv, inv,
     /* 0x38 - 0x3b*/ cmp_r2rm_b, __ref_cmp_r2rm_v, cmp_rm2r_b, cmp_rm2r_v,
     /* 0x3c - 0x3f*/ cmp_i2a_b, cmp_i2a_v, inv, inv,
-    /* 0x40 - 0x43*/ inc_rm_v, inc_rm_v, __ref_inc_rm_v, inc_rm_v,
-    /* 0x44 - 0x47*/ inc_rm_v, inc_rm_v, inc_rm_v, inc_rm_v,
+    /* 0x40 - 0x43*/ __ref_inc_r_v, __ref_inc_r_v, __ref_inc_r_v, __ref_inc_r_v,
+    /* 0x44 - 0x47*/ __ref_inc_r_v, __ref_inc_r_v, __ref_inc_r_v, __ref_inc_r_v,
     /* 0x48 - 0x4b*/ dec_r_v, inv, inv, inv,
     /* 0x4c - 0x4f*/ inv, inv, inv, inv,
     /* 0x50 - 0x53*/ push_r_v, push_r_v, push_r_v, push_r_v,
@@ -113,7 +113,7 @@ instr_func group_3_v_entry[8] =
 
 /* 0xff */
 instr_func group_5_indirect_entry[8] =
-    {inc_rm_v, inv, inv, inv, inv, inv, push_rm_v, inv};
+    {inc_rm_v, inv, inv, inv, inv, inv, __ref_push_rm_v, inv};
 
 instr_func group_7_entry[8] =
     {inv, inv, inv, inv, inv, inv, inv, inv};
