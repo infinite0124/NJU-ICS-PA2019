13649265bb003d070af0aa4e253f49314083b1bf
tracer-pa-public <tracer@njuics.org>
2019-10-25 21:40:00 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  21:40:00 up 2 days,  8:29,  1 user,  load average: 1.01, 0.72, 0.37 8564df6cf07d2a46ee5b9f57f8d7ee367f747c2f

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -4,9 +4,9 @@ static void instr_execute_2op()
 {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
-	printf("opr_src=%x,opr_dest=%x\n",sign_ext(opr_src.val,opr_src.data_size),sign_ext(opr_dest.val,opr_dest.data_size));
+	//printf("opr_src=%x,opr_dest=%x\n",sign_ext(opr_src.val,opr_src.data_size),sign_ext(opr_dest.val,opr_dest.data_size));
 	alu_sub(sign_ext(opr_src.val&(0xffffffff>>(32-data_size)),opr_src.data_size),sign_ext(opr_dest.val&(0xffffffff>>(32-data_size)),opr_dest.data_size),data_size);
-	printf("ZF=%x\n",cpu.eflags.ZF);
+	//printf("ZF=%x\n",cpu.eflags.ZF);
 	//operand_write(&opr_dest);
 }
 make_instr_impl_2op(cmp,i,rm,b)
