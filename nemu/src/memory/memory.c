3685a40b74967c09abc6c59c29e139dbeca571ec
monitor <tracer@njuics.org>
2019-11-23 11:30:01 +0800

    git monitor commit.

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -4,6 +4,7 @@
 #include "device/mm_io.h"
 #include <memory.h>
 #include <stdio.h>
+#include "memory/cache.h"
 
 uint8_t hw_mem[MEM_SIZE_B];
 
@@ -22,13 +23,21 @@ void hw_mem_write(paddr_t paddr, size_t len, uint32_t data)
 uint32_t paddr_read(paddr_t paddr, size_t len)
 {
 	uint32_t ret = 0;
+#ifdef CACHE_ENABLED
+	ret=cache_read (paddr,len,&L1_dcache);
+#else
 	ret = hw_mem_read(paddr, len);
+#endif
 	return ret;
 }
 
 void paddr_write(paddr_t paddr, size_t len, uint32_t data)
 {
+#ifdef CACHE_ENABLED
+	cache_write(paddr,len,data,&L1_dcache);
+#else
 	hw_mem_write(paddr, len, data);
+#endif
 }
 
 uint32_t laddr_read(laddr_t laddr, size_t len)
@@ -57,6 +66,9 @@ void init_mem()
 {
 	// clear the memory on initiation
 	memset(hw_mem, 0, MEM_SIZE_B);
+#ifdef CACHE_ENABLED
+	init_cache();
+#endif
 
 #ifdef TLB_ENABLED
 	make_all_tlb();
