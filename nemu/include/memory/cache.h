0db2b0605f3f73c7b956e93caf577eea97178758
monitor <tracer@njuics.org>
2019-11-23 10:51:01 +0800

    git monitor commit.

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -1,11 +1,11 @@
 struct Cacheline
 {
 	bool valid;//1
-	uint32_t mark;//23
+	uint32_t mark;//26-7=19
 	uint8_t data[64];//64*8
 };
 
-init_cache();
+void init_cache();
 uint32_t cache_read (paddr_t paddr , size_t len , CacheLine *cache);
 void cache_write (paddr_t paddr , size_t len , uint32_t data, CacheLine *cache);
 
