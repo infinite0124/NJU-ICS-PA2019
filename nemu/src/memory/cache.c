52cd0f993e250540c45614fbfa1ebdb82f9b5f24
tracer-pa-public <tracer@njuics.org>
2019-11-24 17:33:39 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  17:33:39 up  4:42,  1 user,  load average: 0.00, 0.00, 0.00 a2debf58d4b9b28b6bd88162cb5e8fd41f24c2e

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -25,7 +25,7 @@ uint32_t cache_read (paddr_t paddr , size_t len , Cacheline *cache)
 	{
 		if(cache[pos].valid&&(cache[pos].mark==sign))//hit
 		{
-			memcpy(&ret,cache[pos].data+8*addr,len);
+			memcpy(&ret,cache[pos].data+addr,len);
 			uint32_t ans=0;
 			memcpy(&ans,hw_mem+paddr,len);
 		//	printf("ans=%x,ret=%x\n",ans,ret);
