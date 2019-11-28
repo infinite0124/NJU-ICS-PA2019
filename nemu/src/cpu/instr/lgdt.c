1fba97226f3cd3785214999e9f4ce58992f8f5ac
tracer-pa-public <tracer@njuics.org>
2019-11-28 20:52:30 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  20:52:30 up  4:36,  1 user,  load average: 0.01, 0.03, 0.00 8441a1536973faa760d628fdb6b4b018d35bcb0

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -4,21 +4,22 @@ make_instr_func(lgdt)
 {
 	OPERAND start;
 	start.type=OPR_IMM;
-	start.data_size=4;
+	start.data_size=32;
 	start.addr=eip+2;
+
 	operand_read(&start);
 	printf("addr=%x\n",start.addr);
 
 	OPERAND limit;
 	limit.type=OPR_IMM;
-	limit.data_size=2;
+	limit.data_size=16;
 	limit.addr=start.val;
 	operand_read(&limit);
 	printf("limit=%x\n",limit.val);
 
 	OPERAND base;
 	base.type=OPR_IMM;
-	base.data_size=4;
+	base.data_size=32;
 	base.addr=start.val+16;
 	operand_read(&base);
 	printf("base=%x\n",base.val);
