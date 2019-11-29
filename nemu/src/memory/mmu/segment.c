0ab173335d115c004bf2daf850c21d3b64ba4892
tracer-pa-public <tracer@njuics.org>
2019-11-29 15:16:00 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  15:16:00 up  3:43,  1 user,  load average: 0.00, 0.00, 0.00 97b820d9c50074aa6ed5302d6c8ffe7f12947c65

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -21,12 +21,13 @@ void load_sreg(uint8_t sreg)
 	SegDesc s;
 	uint32_t addr=cpu.gdtr.base+8*cpu.segReg[sreg].index;
 	printf("addr=%x\n",addr);
-
+	
 	memcpy(&s,(void *)addr,8);
-	cpu.segReg[sreg].base=(s.base_31_24<<24)+(s.base_23_16<<16)+s.base_15_0;
-	cpu.segReg[sreg].limit=(s.limit_19_16<<16)+s.limit_15_0;
-	printf("base=%x\n",cpu.segReg[sreg].base);
-	printf("limit=%x\n",cpu.segReg[sreg].limit);
+	printf("s=%x %x\n",s.val[0],s.val[1]);
+	//cpu.segReg[sreg].base=(s.base_31_24<<24)+(s.base_23_16<<16)+s.base_15_0;
+	//cpu.segReg[sreg].limit=(s.limit_19_16<<16)+s.limit_15_0;
+	//printf("base=%x\n",cpu.segReg[sreg].base);
+	//printf("limit=%x\n",cpu.segReg[sreg].limit);
 
 	assert(s.granularity==1||s.present==0||cpu.segReg[sreg].base==0||cpu.segReg[sreg].limit==0xffffffff);
 
