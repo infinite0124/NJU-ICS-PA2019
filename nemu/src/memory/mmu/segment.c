2640c9d73944e69be0822711920543d86bd836f2
monitor <tracer@njuics.org>
2019-11-29 10:50:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -15,7 +15,13 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg)
 // load the invisible part of a segment register
 void load_sreg(uint8_t sreg)
 {
-	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
+	/* load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
+	uint32_t addr=cpu.gdtr.base+8*cpu.segReg[sreg].index;
+	SegDesc.val=addr;
+	cpu.segReg[sreg].base=SegDesc.base_31_24<<24+SegDesc.base_23_16<<16+SegDesc.base_15_0;
+	cpu.segReg[sreg].limit=SegDesc.limit_19_16<<16+SegDesc.limit_15_0;
+	assert(SegDesc.granularity==1||SegDesc.present==0||cpu.segReg[sreg].base==0||cpu.segReg[sreg].limit==0xffffffff);
+
 }
