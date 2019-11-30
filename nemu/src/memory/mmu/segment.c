d75c307d2e30565df00f597addf96c77d0dd1f4b
monitor <tracer@njuics.org>
2019-11-30 18:06:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -22,7 +22,7 @@ void load_sreg(uint8_t sreg)
 	uint32_t addr=cpu.gdtr.base+8*cpu.segReg[sreg].index;
 	printf("addr=%x\n",addr);
 	
-	memcpy(&s,(void *)addr,8);
+	memcpy(&s,(void *)addr,1);
 	printf("s=%x %x\n",s.val[0],s.val[1]);
 	//cpu.segReg[sreg].base=(s.base_31_24<<24)+(s.base_23_16<<16)+s.base_15_0;
 	//cpu.segReg[sreg].limit=(s.limit_19_16<<16)+s.limit_15_0;
