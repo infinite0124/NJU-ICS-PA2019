b81f7ecf26acfaf74586beaaffbe50b0d1f26daa
tracer-pa-public <tracer@njuics.org>
2019-12-01 17:07:14 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  17:07:14 up  6:17,  1 user,  load average: 0.00, 0.04, 0.06 e1248223f12bda88e291ae575088bf533555a50

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -116,9 +116,11 @@ make_instr_func(mov_c2r_l)
 	reg.type=OPR_REG;
 	reg.addr=modrm.mod;
 	reg.data_size=32;
+	printf("reg=%x,creg=%x\n",reg.addr,creg.addr);
 
 	operand_read(&creg);
 	reg.val=creg.val;
+	printf("reg.val=%x\n",reg.val);
 	operand_write(&reg);
 	return 2;
 }
