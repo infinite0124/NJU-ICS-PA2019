27595d5b8b99e209dfabe50cfada4dee72490136
tracer-pa-public <tracer@njuics.org>
2019-12-01 14:41:59 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  14:41:59 up  3:52,  1 user,  load average: 0.07, 0.08, 0.02 bed97ae9909cbb46dd07d9f61f5d62676a54c8ca

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -22,7 +22,7 @@ void load_sreg(uint8_t sreg)
 	SegDesc s;
 	uint32_t addr=cpu.gdtr.base+cpu.segReg[sreg].index;
 	printf("addr=%x\n",addr);
-	memcpy(&s,cpu.gdtr.base+cpu.segReg[sreg].index,8);
+	memcpy(&s,(void *)addr,8);
 	
 	cpu.segReg[sreg].base=(s.base_31_24<<24)+(s.base_23_16<<16)+s.base_15_0;
 	cpu.segReg[sreg].limit=(s.limit_19_16<<16)+s.limit_15_0;
