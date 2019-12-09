cb9ea2101ce48f931a950776b19540f06fc09289
monitor <tracer@njuics.org>
2019-12-09 11:13:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -8,14 +8,16 @@ paddr_t page_translate(laddr_t laddr)
 	uint32_t dir=laddr>>22;
 	uint32_t page=(laddr&0x003ff000)>>12;
 	uint32_t offset=laddr&0x00000fff;
-	assert(cpu.cr0.pe&&cpu.cr0.pg);
-	uint32_t addr=cpu.cr3.pdtr+4*dir;
-	uint32_t next_addr;
-	memcpy(&next_addr,hw_mem+addr,4);
-	uint32_t p_addr=next_addr+4*page;
-	uint32_t p_page;
-	memcpy(&p_page,hw_mem+p_addr,4);
-	return p_page+offset;
+	assert(cpu.cr0.pe&&cpu.cr0.pg);//
+	uint32_t addr=cpu.cr3.pdtr<<12+4*dir;
+	PDE pde;
+	memcpy(&pde,hw_mem+addr,4);
+	assert(pde.present==1);//
+	uint32_t p_addr=pde.page_frame<<12+4*page;
+	PTE pte;
+	memcpy(&pte,hw_mem+p_addr,4);
+	assert(pte.present==1);//
+	return pte.page_frame<<12+offset;
 	//assert(0);
 #else
 	return tlb_read(laddr) | (laddr & PAGE_MASK);
