0af75f6704c4bcf0d781fabeec34c0ad5b8a930f
monitor <tracer@njuics.org>
2019-12-09 10:46:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -5,7 +5,11 @@
 paddr_t page_translate(laddr_t laddr)
 {
 #ifndef TLB_ENABLED
-	printf("\nPlease implement page_translate()\n");
+	uint32_t dir=laddr>>22;
+	uint32_t page=(laddr&0x003ff000)>>12;
+	uint32_t offset=laddr&0x00000fff;
+	assert(cpu.cr0.pe&&cpu.cr0.pg);
+	uint32_t cpu.cr3.pdtr+4*dir;
 	assert(0);
 #else
 	return tlb_read(laddr) | (laddr & PAGE_MASK);
