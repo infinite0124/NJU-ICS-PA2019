6918d1ccc26d3d30f93d1fc39a88a94bcdaa4fa6
monitor <tracer@njuics.org>
2020-01-11 20:23:01 +0800

    git monitor commit.

---
+++ nemu/src/cpu/instr/lidt.c
@@ -0,0 +1,31 @@
+#include "cpu/instr.h"
+
+make_instr_func(lgdt)
+{
+	OPERAND start;
+	start.type=OPR_IMM;
+	start.data_size=32;
+	start.addr=eip+2;
+	operand_read(&start);
+//	printf("addr=%x\n",start.val);
+
+	OPERAND limit;
+	limit.type=OPR_IMM;
+	limit.data_size=16;
+	limit.addr=start.val;
+	operand_read(&limit);
+//	printf("limit=%x\n",limit.val);
+
+	OPERAND base;
+	base.type=OPR_IMM;
+	base.data_size=32;
+	base.addr=start.val+2;
+	operand_read(&base);
+//	printf("base=%x\n",base.val);
+
+	cpu.gdtr.limit=limit.val;
+	cpu.gdtr.base=base.val;
+
+	return 6;
+
+}
