e5f51784666126accbf7f9cdfbe26554f33c89c1
tracer-pa-public <tracer@njuics.org>
2020-01-12 16:02:29 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  16:02:29 up  2:32,  1 user,  load average: 0.11, 0.12, 0.04 2620bdef3f8f90ef32159d6f51fd025ad1766f83

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -19,7 +19,11 @@ paddr_t page_translate(laddr_t laddr)
 	memcpy(&pte,hw_mem+p_addr,4);
 	assert(pte.present==1);//
 //	assert(pte.page_frame==0);//
-	printf("addr:%x\n",(pte.page_frame<<12)+offset);
+	uint32_t res=(pte.page_frame<<12)+offset;
+	printf("addr:%x\n",res);
+	uint32_t temp;
+	memcpy(&temp,res,4);
+	printf("temp:%x\n",temp);
 	return (pte.page_frame<<12)+offset;
 	//assert(0);
 #else
