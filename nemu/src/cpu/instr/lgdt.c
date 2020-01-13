6c2fffb9bd899c22b295fdd60bd06901d61dbbc9
tracer-pa-public <tracer@njuics.org>
2020-01-13 21:51:44 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  21:51:44 up  1:42,  1 user,  load average: 0.13, 0.13, 0.05 996f37ddc50ac6b19c55d61819b7f1fcfde2e03c

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -2,32 +2,31 @@
 
 make_instr_func(lgdt)
 {
+	int len=2;
 	OPERAND start;
-	modrm_rm(eip+1,&start);
+	len+=modrm_rm(eip+1,&start);
 	start.data_size=32;
-	
-	//operand_read(&start);
-	printf("addr=%x\n",start.mem_addr.disp);
+	//printf("addr=%x\n",start.mem_addr.disp);
 
 	OPERAND limit;
 	limit.type=OPR_IMM;
 	limit.data_size=16;
 	limit.addr=start.mem_addr.disp;
-	printf("limit.addr=%x\n",limit.addr);
+	//printf("limit.addr=%x\n",limit.addr);
 	operand_read(&limit);
-	printf("limit=%x\n",limit.val);
+	//printf("limit=%x\n",limit.val);
 
 	OPERAND base;
 	base.type=OPR_IMM;
 	base.data_size=32;
 	base.addr=start.mem_addr.disp+2;
 	operand_read(&base);
-	printf("base.addr=%x\n",base.addr);
-	printf("base=%x\n",base.val);
+	//printf("base.addr=%x\n",base.addr);
+	//printf("base=%x\n",base.val);
 
 	cpu.gdtr.limit=limit.val;
 	cpu.gdtr.base=base.val;
-
-	return 6;
+	printf("len=%x\n",len);
+	return len;
 
 }
