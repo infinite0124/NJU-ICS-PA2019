2424ca267628d3bc58e26e8f34a74f7fcfc125c6
tracer-pa-public <tracer@njuics.org>
2020-01-14 17:28:49 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  17:28:49 up  4:34,  1 user,  load average: 0.13, 0.09, 0.02 8a677fe1944809a8e8c813e788985ae0ccb14319

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -8,7 +8,14 @@ void raise_intr(uint8_t intr_no)
 	pushf();
 	push_segReg(1);
 	push_eip();
-
+	assert(intr_no<=cpu.idtr.limit);
+	printf("base=%x,limit=%x\n",cpu.idtr.base,cpu.idtr.limit);
+	uint32_t addr=hw_mem+cpu.idtr.base+8*intr_no;
+	print("addr=%x\n",addr);
+	GateDesc gatedesc;
+	memcpy(&gatedesc,addr,8);
+	
+	
 #endif
 }
 
