d00c2f597eb3f76133ee817b9b06a22ccad7e96f
monitor <tracer@njuics.org>
2020-01-14 19:14:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -5,25 +5,24 @@ make_instr_func(lgdt)
 	int len=1;
 	OPERAND start;
 	len+=modrm_rm(eip+1,&start);
-	start.data_size=32;
-	operand_read(&start);
-	printf("addr=%x\n",start.val);
+	
+	//printf("addr=%x\n",start.addr);
 
 	OPERAND limit;
 	limit.type=OPR_IMM;
 	limit.data_size=16;
 	limit.addr=start.addr;
-	printf("limit.addr=%x\n",limit.addr);
+	//printf("limit.addr=%x\n",limit.addr);
 	operand_read(&limit);
-	printf("limit=%x\n",limit.val);
+	//printf("limit=%x\n",limit.val);
 
 	OPERAND base;
 	base.type=OPR_IMM;
 	base.data_size=32;
 	base.addr=start.addr+2;
 	operand_read(&base);
-	printf("base.addr=%x\n",base.addr);
-	printf("base=%x\n",base.val);
+	//printf("base.addr=%x\n",base.addr);
+	//printf("base=%x\n",base.val);
 
 	cpu.gdtr.limit=limit.val;
 	cpu.gdtr.base=base.val;
