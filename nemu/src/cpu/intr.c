143fdfd0f6e86f4cb3c8b93f88e7386e6de5b8d0
monitor <tracer@njuics.org>
2020-01-14 19:24:01 +0800

    git monitor commit.

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -9,14 +9,14 @@ void raise_intr(uint8_t intr_no)
 	push_segReg(1);
 	push_eip();
 	assert(intr_no<=cpu.idtr.limit);
-	printf("base=%x,limit=%x\n",cpu.idtr.base,cpu.idtr.limit);
-	printf("base=%x,limit=%x\n",cpu.gdtr.base,cpu.gdtr.limit);
+	//printf("base=%x,limit=%x\n",cpu.idtr.base,cpu.idtr.limit);
+	//printf("base=%x,limit=%x\n",cpu.gdtr.base,cpu.gdtr.limit);
 
 	uint32_t addr=cpu.idtr.base+8*intr_no;
 	printf("addr=%x\n",addr);
 	GateDesc gatedesc;
 	memcpy(&gatedesc,hw_mem+addr,8);
-	
+	printf("type:%x\n",gatedesc.type);
 	
 #endif
 }
