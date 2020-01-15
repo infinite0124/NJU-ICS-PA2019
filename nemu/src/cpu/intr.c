8e1fe4def366cf43053c02fbfb952b03fbeb7b0d
tracer-pa-public <tracer@njuics.org>
2020-01-15 12:25:55 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  12:25:55 up 6 min,  1 user,  load average: 0.15, 0.14, 0.07 5e236a04bdea59f69f526764ae1ad852602df6b

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,19 +5,26 @@
 void raise_intr(uint8_t intr_no)
 {
 #ifdef IA32_INTR
-	pushf();
-	push_segReg(1);
-	push_eip();
-	printf("eip=%x\n",cpu.eip);
+//	pushf();
+//	push_segReg(1);
+//	push_eip();
+//	printf("eip=%x\n",cpu.eip);
 	assert(intr_no<=cpu.idtr.limit);
 	//printf("base=%x,limit=%x\n",cpu.idtr.base,cpu.idtr.limit);
 	//printf("base=%x,limit=%x\n",cpu.gdtr.base,cpu.gdtr.limit);
+	cpu.esp-=4;
+	vaddr_write(cpu.esp,SREG_SS,4,cpu.eflags.val);
+	cpu.esp-=2;
+	vaddr_write(cpu.esp,SREG_SS,2,cpu.cs.val);
+	cpu.esp-=4;
+	vaddr_write(cpu.esp,SREG_SS,4,cpu.eip);
 
-	uint32_t addr=cpu.idtr.base+8*intr_no;
+	uint32_t addr=segment_translate(cpu.idtr.base+8*intr_no,2);
+	uint32_t paddr=page_translate(addr);
 	//printf("intr_no=%x\n",intr_no);
 	//printf("addr=%x\n",addr);
 	GateDesc gatedesc;
-	memcpy(&gatedesc,hw_mem+addr-0xc0000000,8);
+	memcpy(&gatedesc,hw_mem+paddr,8);
 	printf("type:%x\n",gatedesc.type);
 	if(gatedesc.type==0xe)
 		cpu.eflags.IF=0;
