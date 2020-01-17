d7fd8f8e907ead89e753e7fdd8650af623c24b5e
monitor <tracer@njuics.org>
2020-01-17 17:34:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/in_out.c
+++ nemu/src/cpu/instr/in_out.c
@@ -3,30 +3,30 @@
 
 make_instr_func(in_b)
 {
-	pio_read(cpu.edx._16,1);
+	pio_read(cpu.edx_16,1);
 	return 1;
 }
 
 make_instr_func(in_v)
 {
 	if(data_size==16)
-		pio_read(cpu.edx._16,2);
+		pio_read(cpu.edx_16,2);
 	else if(data_size==32)
-		pio_read(cpu.edx._16,4);
+		pio_read(cpu.edx_16,4);
 	return 1;
 }
 
 make_instr_func(out_b)
 {
-	pio_write(cpu.edx._16,1,cpu.eax._8[0]);
+	pio_write(cpu.edx_16,1,cpu.eax_8[0]);
 	return 1;
 }
 
 make_instr_func(out_v)
 {
 	if(data_size==16)
-		poi_write(cpu.edx._16,2,cpu.eax._16);
+		pio_write(cpu.edx_16,2,cpu.eax_16);
 	else if(data_size==32)
-		poi_write(cpu.edx._16,4,cpu.eax._32);
+		poi_write(cpu.edx_16,4,cpu.eax_32);
 	return 1;
 }
