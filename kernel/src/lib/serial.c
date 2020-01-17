71e7fa920bbf8e2e13ddca3daae1063016927635
tracer-pa-public <tracer@njuics.org>
2020-01-17 16:38:48 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  16:38:48 up 52 min,  1 user,  load average: 0.58, 0.33, 0.15 d4d8b6233fe792c548e81064f83e3024102f34a

--- kernel/src/lib/serial.c
+++ kernel/src/lib/serial.c
@@ -13,10 +13,10 @@ serial_idle(void)
 
 void serial_printc(char ch)
 {
-	Log("%x ",ch);
+	//Log("%x ",ch);
 	while (!serial_idle())
 		out_byte(0x3f8,ch); // wait untile serial is idle
-	//out_byte(0x3f8,ch);
+	out_byte(0x3f8,ch);
 	// print 'ch' via out instruction here
 	//HIT_BAD_TRAP;
 }
