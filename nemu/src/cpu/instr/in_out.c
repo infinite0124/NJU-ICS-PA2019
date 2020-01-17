dc31d628e8b5ba7a30e4c882f436d8d1225bec86
tracer-pa-public <tracer@njuics.org>
2020-01-17 17:30:33 +0800

    >  nemu 181860154 zq Linux debian10 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  17:30:33 up  1:44,  1 user,  load average: 0.66, 0.29, 0.22 6023f8fc89f4edf692e992955a5dbf4f3b06b4ba

--- nemu/src/cpu/instr/in_out.c
+++ nemu/src/cpu/instr/in_out.c
@@ -2,20 +2,30 @@
 
 make_instr_func(in_b)
 {
+	pio_read(cpu.edx._16,1);
 	return 1;
 }
 
 make_instr_func(in_v)
 {
+	if(data_size==16)
+		pio_read(cpu.edx._16,2);
+	else if(data_size==32)
+		pio_read(cpu.edx._16,4);
 	return 1;
 }
 
 make_instr_func(out_b)
 {
+	pio_write(cpu.edx._16,1,cpu.eax._8[0]);
 	return 1;
 }
 
 make_instr_func(out_v)
 {
+	if(data_size==16)
+		poi_write(cpu.edx._16,2,cpu.eax._16);
+	else if(data_size==32)
+		poi_write(cpu.edx._16,4,cpu.eax._32);
 	return 1;
 }
